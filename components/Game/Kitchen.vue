231e4cdf7a7d056272c8bae24231af10d275a741
Eliska <eliska@pixelhop.io>
2024-04-07 19:37:57 +0100

    Animate selected bottle

--- components/Game/Kitchen.vue
+++ components/Game/Kitchen.vue
@@ -6,6 +6,7 @@ import {
   type Object3DEventMap,
 } from "three/src/Three.js";
 import { useEventListener } from "@vueuse/core";
+import anime from "animejs";
 
 const emit = defineEmits<{
   (e: "selected-bottle", bottle: string): void;
@@ -38,8 +39,13 @@ const bottles = model.children.filter((child: any) =>
 );
 bottles.forEach((item: Object3D<Object3DEventMap>) => {
   item.castShadow = true;
+
+  item.initialRotationZ = item.rotation.z;
+  item.initialPositionY = item.position.y;
 });
 
+let animation: any;
+let previousSelection = ref();
 const selectedBottle = ref();
 useEventListener(document, "click", (event) => {
   if (!camera.value) {
@@ -54,8 +60,36 @@ useEventListener(document, "click", (event) => {
   const modelIntersects = raycaster.value.intersectObjects(bottles);
 
   if (modelIntersects.length) {
-    selectedBottle.value = modelIntersects[0].object.name;
-    emit("selected-bottle", selectedBottle.value);
+    selectedBottle.value = modelIntersects[0].object;
+    emit("selected-bottle", selectedBottle.value.name);
+
+    // a new bottle clicked and another bottle was clicked previously
+    if (
+      animation &&
+      previousSelection.value?.name !== selectedBottle.value?.name
+    ) {
+      animation.pause();
+      anime({
+        targets: previousSelection.value.position,
+        y: previousSelection.value.initialPositionY,
+        easing: "easeOutQuad",
+        duration: 150,
+      });
+    }
+
+    // a new bottle was clicked
+    if (previousSelection.value?.name !== selectedBottle.value.name) {
+      animation = anime({
+        targets: selectedBottle.value.position,
+        y: selectedBottle.value.initialPositionY + 0.05,
+        duration: 1000,
+        easing: "linear",
+        direction: "alternate",
+        loop: true,
+      });
+    }
+
+    previousSelection.value = modelIntersects[0].object;
   }
 });
 </script>
