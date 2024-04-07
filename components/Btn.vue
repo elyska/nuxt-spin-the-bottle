7b816fd43001057136cbf485b79c2a3863f9c787
Eliska <eliska@pixelhop.io>
2024-04-07 14:50:23 +0100

    Tailwind, button component

--- components/Btn.vue
+++ components/Btn.vue
@@ -1,5 +1,68 @@
+<script setup lang="ts">
+import type { ButtonTheme } from "~/types/button.types";
+
+const props = withDefaults(
+  defineProps<{
+    theme?: ButtonTheme;
+  }>(),
+  {
+    theme: "primary",
+  }
+);
+
+const btnClasses = computed(() => {
+  const themeClasses = {
+    primary: "bg-green-800",
+    error: "bg-red-800",
+  };
+
+  return themeClasses[props.theme];
+});
+
+const spanClasses = computed(() => {
+  const themeClasses = {
+    primary: "bg-green-500/[0.7]",
+    error: "bg-red-500/[0.7]",
+  };
+
+  return themeClasses[props.theme];
+});
+</script>
+
 <template>
-  <button>
-    <slot />
+  <button
+    type="button"
+    id="play"
+    class="pushable drop-shadow-md"
+    :class="btnClasses"
+  >
+    <span
+      class="font-bangers tracking-wider text-2xl pushable-front"
+      :class="spanClasses"
+    >
+      <slot />
+    </span>
   </button>
 </template>
+
+<style scoped>
+.pushable {
+  border-radius: 12px;
+  border: none;
+  padding: 0;
+  cursor: pointer;
+  outline-offset: 4px;
+}
+.pushable-front {
+  display: block;
+  padding: 12px 42px;
+  border-radius: 12px;
+  color: white;
+  transform: translateY(-6px);
+  transition: transform 0.2s;
+}
+
+.pushable:active .pushable-front {
+  transform: translateY(-2px);
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  app: {
+    head: {
+      link: [
+        { rel: "preconnect", href: "https://fonts.googleapis.com" },
+        {
+          rel: "preconnect",
+          href: "https://fonts.gstatic.com",
+          crossorigin: "",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Onest:wght@100..900&display=swap",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Bangers&display=swap",
+        },
+      ],
+    },
+  },
 })
